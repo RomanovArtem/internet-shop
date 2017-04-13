1e96998910f216f5ca8127fcbdf8a03f7f928a3e
Romanov Artem <temik_romanov@mail.ru>
2017-04-13 19:50:16 +0300

    Добавил уведомления о новых заказах и отзывах

--- www/admin/include/block-header.php
+++ www/admin/include/block-header.php
@@ -1,5 +1,25 @@
 <?php
 	defined('myshop') or die('Доступ запрещён!');
+    
+    $resultNoConfirmedOrders = mysql_query("SELECT * FROM orders WHERE order_confirmed='no'", $link);
+    $countNoConfirmedOrders = mysql_num_rows($resultNoConfirmedOrders);
+    
+    if ($countNoConfirmedOrders > 0) {
+        $count_str1 = '<p>+'.$countNoConfirmedOrders.'</p>';
+    }
+    else {
+        $count_str1 = '';
+    }
+    
+    $resultNoApprovedReviews = mysql_query("SELECT * FROM table_reviews WHERE approved='0'", $link);
+    $countNoApprovedReviews = mysql_num_rows($resultNoApprovedReviews);
+    
+    if ($countNoApprovedReviews > 0) {
+        $count_str2 = '<p>+'.$countNoApprovedReviews.'</p>';
+    }
+    else {
+        $count_str2 = '';
+    }
 ?>
 <div class="block-header">
 	<div class = "block-header-left">
@@ -14,9 +34,9 @@
 
 <div class="left-nav">
 	<ul>
-		<li><a href="orders.php">Заказы</a></li>
+		<li><a href="orders.php">Заказы</a><?php echo $count_str1; ?></li>
 		<li><a href="products.php">Товары</a></li>
-		<li><a href="reviews.php">Отзывы</a></li>
+		<li><a href="reviews.php">Отзывы</a><?php echo $count_str2; ?></li>
 		<li><a href="category.php">Категории</a></li>
 		<li><a href="clients.php">Клиенты</a></li>
 		<li><a href="news.php">Новости</a></li>
